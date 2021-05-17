85672d519829f1d2cb51fe1462aae4bf94a7050f
DIVYASHEK K <78641652+divyashek6267@users.noreply.github.com>
2021-05-17 12:33:02 +0530

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,37 @@
+class Food{
+    constructor(){
+    }
+   
+
+
+    display(){
+    fill("red")
+
+      var button=createButton("Feed the Dog");
+      button.position(400,125);
+
+      if(button.mousePressed(function(){
+         foodS=foodS-1; 
+         gameState=1;
+         database.ref('/').update({'gameState':gameState});
+      }));
+
+      var addFood=createButton("Add Food");
+      addFood.position(630,125);
+
+      if(addFood.mousePressed(function(){
+        foodS=foodS+1;
+        gameState=2;
+        database.ref('/').update({'gameState':gameState});
+      }));
+    }
+
+ 
+      
+      
+       
+
+       
+    }
+
+  
